3930aab561a32d20e1e0074b9da7e6803660469b
mohammedalmostfa <mohammedalmostfa36@gmail.com>
2024-10-30 01:02:19 +0300

    forth  commit

--- Daily task management system/app/Console/Kernel.php
+++ Daily task management system/app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('app:check-task')->dailyAt('15:37');
+        $schedule->command('app:check-task')->dailyAt('23:59');
 
     }
 
